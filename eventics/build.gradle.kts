04fc47551bca39cc5e31b86ef037fdbcdb59a6cb
Meet Miyani <91192898+Meet-Miyani@users.noreply.github.com>
2024-08-31 13:03:48 +0530

    Changing publishing plugin

--- eventics/build.gradle.kts
+++ eventics/build.gradle.kts
@@ -1,6 +1,9 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("com.vanniktech.maven.publish") version "0.29.0"
 }
 
 group = "io.github.meet-miyani"
@@ -11,7 +14,7 @@ android {
     compileSdk = 34
 
     defaultConfig {
-        minSdk = 16
+        minSdk = 19
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
@@ -37,4 +40,44 @@ android {
 
 dependencies {
     implementation(libs.androidx.core.ktx)
+}
+
+mavenPublishing {
+    coordinates(
+        groupId = "io.github.meet-miyani",
+        artifactId = "eventics-library",
+        version = "1.0.0",
+    )
+
+    pom {
+        name.set("Eventics Library")
+        description.set("A library for managing and logging events in Android applications.")
+        inceptionYear.set("2024")
+        url.set("https://github.com/Meet-Miyani/Eventics")
+
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+
+        developers {
+            developer {
+                id.set("meet-miyani")
+                name.set("Meet Miyani")
+                email.set("miyanimeet02@gmail.com")
+            }
+        }
+
+        scm {
+            connection.set("scm:git:git://github.com/Meet-Miyani/Eventics.git")
+            developerConnection.set("scm:git:ssh://github.com:Meet-Miyani/Eventics.git")
+            url.set("https://github.com/Meet-Miyani/Eventics")
+        }
+    }
+
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+
+    signAllPublications()
 }
